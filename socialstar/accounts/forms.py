9f56249fc9268a73d001e9ba9ff0fdf038b1731d
Arif <arifrasim1@gmail.com>
2021-08-21 20:41:40 +0300

    Refactored templates

--- socialstar/accounts/forms.py
+++ socialstar/accounts/forms.py
@@ -14,7 +14,3 @@ class UserCreateForm(UserCreationForm):
         self.fields['password1'].help_text = None
         self.fields['password2'].help_text = None
         self.fields['email'].label = 'Email Address'
-        # self.fields['username'].widget.attrs.update({'class': 'form-control'})
-        # self.fields['email'].widget.attrs.update({'class': 'form-control'})
-        # self.fields['password1'].widget.attrs.update({'class': 'form-control'})
-        # self.fields['password2'].widget.attrs.update({'class': 'form-control'})
